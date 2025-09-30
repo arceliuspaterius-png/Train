295e151797a33d64a01f2ffce609b7c7b3fb0bfc
Marcelius <arceliuspaterius@gmail.com>
2025-09-30 16:26:57 +0200

    Viva la HTML
    :shrug:

---
+++ Raag/py.py
@@ -0,0 +1,9 @@
+# lista = ["abc", "ala", 123, True]
+#          0      1     2     3
+# print(lista[-1])
+# print(lista[1:3])
+# listaPięcUlubionychZwiezląt = ["Koteł", "HOI4 cavarly 3d horse div", "Pociąg parowy", "piseł", "Bydgoszsc"]
+# print(listaPięcUlubionychZwiezląt[1][16:24]) 
+listaPięcUlubionychZwiezląt = ["Koteł", "HOI4 cavarly 3d horse div", "Pociąg parowy", "piseł", "Bydgoszsc"]
+print(listaPięcUlubionychZwiezląt[1][12:14]) 
+print(listaPięcUlubionychZwiezląt[1][1::])
